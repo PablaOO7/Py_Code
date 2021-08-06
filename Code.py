49b08efb4995d04f3cdd719eb5679ba3708ac24f
PablaOO7 <57266535+PablaOO7@users.noreply.github.com>
2021-08-06 15:48:37 +0530

    Jas_internship_code

---
+++ Code.py
@@ -0,0 +1,6 @@
+name = "Jaspreet"
+email = "pablajaspreet94@gmail.com"
+slack_User = "@Jaspreet"
+biostack = "Genomics"
+ 
+print ("Name: "+name, "Email: "+email, "Slack Username: " +slack_User, "Biostack: "+biostack, sep = "\n")
